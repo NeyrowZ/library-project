eb9f4df706c67c9beb7939c9df10b841c108945a
NeyrowZ <neyrowz@outlook.fr>
2025-02-10 14:33:38 +0100

    CSS

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,12 +5,16 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\BookRepository;
 
 final class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(BookRepository $bookRepo): Response
     {
-        return $this->render('home/index.html.twig');
+        $books = $bookRepo->findAll();
+        return $this->render('home/index.html.twig', [
+            'books' => $books
+        ]);
     }
 }
